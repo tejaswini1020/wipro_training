780d3b12c3a6494db5ac3a86e0458fa72a02b002
KANTABATHINATEJASWINI <tejaswini102030@gmail.com>
2025-01-31 15:46:02 +0530

    firstprogram

--- Java/src/main/java/sample.java
+++ Java/src/main/java/sample.java
@@ -1,10 +1,7 @@
 
 public class sample {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String [] args) {
 		System.out.println("Hello world");
-
 	}
 
 }
